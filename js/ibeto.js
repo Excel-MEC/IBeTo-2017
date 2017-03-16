d2d6a9980d90306e162d357e287f1b7db9c8fe3a
ashwinkk <ashwinkailas@gmail.com>
2017-03-16 22:06:05 +0530

    parallax trial

--- js/ibeto.js
+++ js/ibeto.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   // tab_handler();
+  console.log('ready');
   $(window).scroll(function(){
-    $('.para1').css('transform':'translateY('+$(window).scrollTop()/10+'px)');
+    console.log('scrolldet');
+    $('.para1').css({'transform':'translate3d(0px,-'+$(window).scrollTop()/12+'px,0px)'});
+    $('.para2').css({'transform':'translate3d(0px,-'+$(window).scrollTop()/10+'px,0px)'});
+    $('.para3').css({'transform':'translate3d(0px,-'+$(window).scrollTop()/6+'px,0px)'});
+    $('.para4').css({'transform':'translate3d(0px,-'+$(window).scrollTop()/3+'px,0px)'});
+    $('.para5').css({'transform':'translateY(-'+$(window).scrollTop()/2+'px)'});
+    $('.para6').css({'transform':'translateY(-'+$(window).scrollTop()+'px)'});
+    $('.para7').css({'transform':'translateY(-'+$(window).scrollTop()+'px)'});
   });
 });
