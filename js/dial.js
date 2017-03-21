5c7cb217d1b291e51feee3bf01c203dfb9481432
ashwinkk <ashwinkailas@gmail.com>
2017-03-21 14:30:53 +0530

    circular scroll

--- js/dial.js
+++ js/dial.js
@@ -1,54 +1,64 @@
 function trigger() {
-	var rotateFactor=1;
-	//var dprocess = $.debounce(process, 1600);
+	window.rotateFactor=1;
+	$('body').on('mousewheel',handleScroll);
 
-	$('body').on('mousewheel',function(event,delta){
-		// console.log('ess');
-		if (rotateFactor==1) {
+}
+
+function handleScroll(event,delta){
+	$(this).off('mousewheel');
+	// console.log(delta);
+	if (rotateFactor==1) {
+		if(delta<0){
 			$('#intro').css({'transform':'translate(0,-50%) rotate(-180deg)'});
 			$('#hack').css({'transform':'translate(0,-50%) rotate(0deg)'});
-			// $('#intro').addClass('animate-tohidden-fwd');
-			// $('#intro').addClass('initial');
-			// $('#hack').addClass('animate-tocurr-fwd');
-			// $('#hack').removeClass('initial');
-			setTimeout(function(){
-			// $('#intro').removeClass('animate-tohidden-fwd');
-			// $('#hack').removeClass('animate-tocurr-fwd');
-			rotateFactor=2;
-			console.log("2");
-		},1200);
+				rotateFactor=2;
 		}
-		else if (rotateFactor==2) {
+		setTimeout(function(){
+			console.log(rotateFactor);
+			$(this).on('mousewheel',handleScroll);
+		},1020);
+	}
+	else if (rotateFactor==2) {
+		if(delta<0){
 			$('#hack').css({'transform':'translate(0,-50%) rotate(-180deg)'});
 			$('#rules').css({'transform':'translate(0,-50%) rotate(0deg)'});
-			// $('#hack').addClass('animate-tohidden-fwd');
-			// $('#hack').addClass('initial');
-			// $('#rules').addClass('animate-tocurr-fwd');
-			// $('#rules').removeClass('initial');
-			setTimeout(function(){
-			// 	$('#hack').removeClass('animate-tohidden-fwd');
-			// 	$('#rules').removeClass('animate-tocurr-fwd');
-				rotateFactor=3;
-				console.log("3");
-			//
-			},1200);
+			rotateFactor=3;
 		}
-		else if (rotateFactor==3) {
+		else{
+			$('#hack').css({'transform':'translate(0,-50%) rotate(180deg)'});
+			$('#intro').css({'transform':'translate(0,-50%) rotate(0deg)'});
+			rotateFactor=1;
+		}
+		setTimeout(function(){
+			console.log(rotateFactor);
+			$(this).on('mousewheel',handleScroll);
+		},1020);
+	}
+	else if (rotateFactor==3) {
+		if(delta<0){
 			$('#rules').css({'transform':'translate(0,-50%) rotate(-180deg)'});
 			$('#time').css({'transform':'translate(0,-50%) rotate(0deg)'});
-			// $('#rules').addClass('animate-tohidden-fwd');
-			// $('#rules').addClass('initial');
-			// $('#time').addClass('animate-tocurr-fwd');
-			// $('#time').removeClass('initial');
-			setTimeout(function(){
-			// 	$('#rules').removeClass('animate-tohidden-fwd');
-			// 	$('#time').removeClass('animate-tocurr-fwd');
-				rotateFactor=4;
-				console.log("4");
-			//
-			},1200);
+			rotateFactor=4;
 		}
-
-	});
-
+		else{
+			$('#rules').css({'transform':'translate(0,-50%) rotate(180deg)'});
+			$('#hack').css({'transform':'translate(0,-50%) rotate(0deg)'});
+			rotateFactor=2;
+		}
+		setTimeout(function(){
+			console.log(rotateFactor);
+			$(this).on('mousewheel',handleScroll);
+		},1020);
+	}
+	else if (rotateFactor==4){
+		if(delta>0){
+			$('#time').css({'transform':'translate(0,-50%) rotate(180deg)'});
+			$('#rules').css({'transform':'translate(0,-50%) rotate(0deg)'});
+			rotateFactor=3;
+		}
+		setTimeout(function(){
+			console.log(rotateFactor);
+			$(this).on('mousewheel',handleScroll);
+		},1020);
+	}
 }
